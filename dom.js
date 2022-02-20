0a9de97f72967337a909dd4129447f7818a29b1b
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-02-20 11:16:41 +0600

    es6-intro

---
+++ dom.js
@@ -0,0 +1,4 @@
+    document.getElementById('add-border').addEventListener('click', function(){
+const container = document.getElementById('friend-container');
+container.style.border='3px solid yellow'
+    })
