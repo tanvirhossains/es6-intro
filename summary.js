0c90b715008ce20b6a483c411cfbee38fff6717b
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-02-26 12:14:38 +0600

    adding

--- summary.js
+++ summary.js
@@ -1,5 +1,13 @@
-// module 31 -9
-function getName (first, second){
+// module 31 -9  (module summary and ES6 racap)
+
+let priyoperson = 'Koolsum Begum';
+priyoperson = 'Momotaz Begom'
+const hubby  = 'akbar the great'
+
+//default parameter 
+
+
+function getName (first, second ){
     return 
 }
 const myPeople = `My lovely person is are ${hubby} and the also ${}`
